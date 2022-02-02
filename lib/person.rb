87f7798e9f9b8e3fccf57581efe5c02094f3a18d
Gehrig-Barnes <gehrigbarnes@gmail.com>
2022-02-02 09:56:43 -0600

    commit tree 7bdb0340454aceb971545e0810cff72f0e481ecf

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+    attr_accessor :happiness
+
+    def initialize(name, bank_account=25, happiness=8)
+       
+    end
